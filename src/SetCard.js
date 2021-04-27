85e3b35a33cc2eaa1c9a056743613264234d51fe
Adrian Hall <muzioclavecin@gmail.com>
2021-04-27 15:06:29 +0200

    Add aria-label to icon button

--- src/SetCard.js
+++ src/SetCard.js
@@ -18,7 +18,7 @@ function DotsMenu(props) {
   const [open, setOpen] = useState(false);
   
   return <OutsideClickDetector className="DotsMenu" onClickOutside={() => setOpen(false)} active={open}>
-    <button className="DotsMenu__button" onClick={() => setOpen(!open)}>
+    <button className="DotsMenu__button" onClick={() => setOpen(!open)} aria-label="Actions Menu">
       <svg width="24" xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z" />
       </svg>
